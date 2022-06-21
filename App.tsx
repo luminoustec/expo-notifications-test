30d90ba869e41383a544a1bbcaa6d17af6ce68bc
Vadim Pishchenko <vadim@luminoustec.net>
2022-06-21 12:15:36 -0700

    slimmed app tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import * as Device from 'expo-device';
 import React, { useEffect } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
+import { Text } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import notifee from '@notifee/react-native';
 
@@ -56,34 +55,3 @@ export default function App() {
     return <Text>Frontpage</Text>;
 }
 
-async function registerForPushNotificationsAsync() {
-    let token;
-    if (Device.isDevice) {
-        const { status: existingStatus } =
-            await Notifications.getPermissionsAsync();
-        let finalStatus = existingStatus;
-        if (existingStatus !== 'granted') {
-            const { status } = await Notifications.requestPermissionsAsync();
-            finalStatus = status;
-        }
-        if (finalStatus !== 'granted') {
-            alert('Failed to get push token for push notification!');
-            return;
-        }
-        token = (await Notifications.getDevicePushTokenAsync()).data;
-        console.log(token);
-    } else {
-        alert('Must use physical device for Push Notifications');
-    }
-
-    if (Platform.OS === 'android') {
-        Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-        });
-    }
-
-    return token;
-}
